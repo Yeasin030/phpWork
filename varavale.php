f4287eca8685685d568f491d69b096715f54434f
Yeasin030 <yeasinarafat5331@gmail.com>
2026-09-19 12:09:18 +0600

    var dump

--- varavale.php
+++ varavale.php
@@ -3,7 +3,17 @@
 $x = 30;
 $y = "Hello";
 //echo $x;
-print $y;
+//print $y;
+$flowers = array("Rose","Tulip","Sunflowers") ;
+echo "<pre>";
+var_dump( $flowers);
+$number=12;
+var_dump($number);
+$number1='a';
+var_dump($number1);
+$number2=12.23;
+var_dump($number2);
+
 
 
 ?>
