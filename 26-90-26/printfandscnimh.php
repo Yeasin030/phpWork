6390f38fb8171cf87d82396076969af21c610e4e
Yeasin030 <yeasinarafat5331@gmail.com>
2026-09-26 12:30:23 +0600

    printf

--- 26-90-26/printfandscnimh.php
+++ 26-90-26/printfandscnimh.php
@@ -1,5 +1,10 @@
+<a href="printfandscnimh.php?name='yeasin'&do='Codinf'&live='Narayangonj'">Clik here</a>
 <?php
 //printf("Hai %d you",2938);
-sprintf("Hai %d you",2938);
-
+// sprintf("Hai %d you",2938);
+echo"<pre>";
+//print_r($_SERVER); //supre global array
+//echo $_SERVER['REQUEST_METHOD'];
+//print_r($_GET);
+echo $_GET['name'];
 ?>
